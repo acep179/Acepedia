8725593a591aaec85e89b0e0e8d01aed7f81570c
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-09-21 00:26:36 +0700

    inisiasi awal

---
+++ src/pages/Products.jsx
@@ -0,0 +1,15 @@
+import { NavBar, SideBar } from './../components'
+
+function Products() {
+  return (
+    <div>
+      <NavBar />
+      <SideBar />
+      <div>
+        <p>This is Products's Page</p>
+      </div>
+    </div>
+  )
+}
+
+export default Products
