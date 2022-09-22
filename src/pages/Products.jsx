4ac98b831a06f94c4532917ec02d48f7ab176d40
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-09-23 01:11:19 +0700

    Menambahkan fitur search product di sisi client

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,6 +7,7 @@ import { useState } from "react"
 function Products() {
 
   const [productData, setProductData] = useState({})
+  const [productName, setProductName] = useState('')
 
   const showModal = (item, type) => {
     setProductData(item)
@@ -23,6 +24,10 @@ function Products() {
     deleteProductModal.style.display = 'flex'
   }
 
+  let searchProduct = products.filter((item) => {
+    return item.name.toLowerCase().match(productName.toLowerCase())
+  })
+
   return (
     <div className="mb-5 mx-auto">
       <div className='flex justify-between items-center mb-5'>
@@ -34,7 +39,7 @@ function Products() {
               <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                 <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
               </div>
-              <input type="text" id="table-search" className="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items" />
+              <input type="text" id="table-search" className="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items" onChange={(e) => setProductName(e.target.value)} />
             </div>
           </div>
         </div>
@@ -55,7 +60,34 @@ function Products() {
           </tr>
         </thead>
         <tbody>
-          {products.map((item, index) => {
+          {searchProduct.length === 0 ? products.map((item, index) => {
+            return (
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={index}>
+                <td className="text-center">{index + 1}</td>
+                <td className="py-3"><img className="h-20" src={item.image} alt={item.name} /></td>
+                <td className="py-4 px-6 font-semibold text-gray-900 dark:text-white">{item.name}</td>
+                <td className="text-center">{item.purchasePrice}</td>
+                <td className="text-center">{item.sellingPrice}</td>
+                <td className="text-center">{item.qty}</td>
+                <td className="text-center">
+                  <button className='bg-red-500 hover:bg-red-600 px-2 py-1 ml-auto mr-1 text-white text-sm rounded-md group relative' onClick={() => showModal(item, 'delete')}>
+                    <IoTrashBin className="w-5 h-5" />
+                    <div className='hidden group-hover:block ml-2 absolute bg-red-200 w-max text-slate-600 px-2 py-1 rounded-md -bottom-10 left-0 z-10'>
+                      <IoTriangle className="w-6 h-6 absolute -top-4 left-0 fill-red-200" />
+                      <p className='font-semibold'>Remove Product</p>
+                    </div>
+                  </button>
+                  <button className='bg-emerald-500 hover:bg-emerald-600 px-2 py-1 mr-auto ml-1 text-white text-sm rounded-md group relative' onClick={() => showModal(item, 'edit')}>
+                    <AiOutlineEdit className="w-5 h-5" />
+                    <div className='hidden group-hover:block ml-2 absolute bg-emerald-200 w-max text-slate-600 px-2 py-1 rounded-md -bottom-10 left-0 z-10'>
+                      <IoTriangle className="w-6 h-6 absolute -top-4 left-0 fill-emerald-200" />
+                      <p className='font-semibold'>Edit Product</p>
+                    </div>
+                  </button>
+                </td>
+              </tr>
+            )
+          }) : searchProduct.map((item, index) => {
             return (
               <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={index}>
                 <td className="text-center">{index + 1}</td>
